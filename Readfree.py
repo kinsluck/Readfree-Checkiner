32f20e2c445b008e2c5a1f25488b0ba1f5a5a0ea
kinsuck <kinsluck@gmail.com>
2018-01-21 19:54:28 +0800

    Upload the script.Based on python3.

---
+++ Readfree.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+try:
+    import cookielib
+except:
+    import http.cookiejar as cookielib
+try:
+    from PIL import Image
+    from bs4 import BeautifulSoup
+except:
+    pass
+
+# 构造 Request headers
+agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'
+headers = {
+    "Host": "readfree.me",
+    "Referer": "http://readfree.me/accounts/login/?next=/",
+    'User-Agent': agent
+}
+
+# Get the soup and ready for cookie
+all_url = 'http://readfree.me/accounts/login/?next=/'
+s = requests.session()
+s.cookies = cookielib.LWPCookieJar(filename='cookies')
+try:
+    s.cookies.load(ignore_discard=True)
+except IOError:
+    print('Cookie未加载！')
+
+rq = s.get(url=all_url, headers=headers)
+soup = BeautifulSoup(rq.text, 'lxml')
+
+
+def get_csrf():
+    _csrf = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+    return _csrf
+
+
+def get_captcha1():
+    curl = soup.find('img', {'class': 'captcha'})['src']
+    captcha_url = 'http://readfree.me/captcha/image' + curl
+    response = s.get(captcha_url, headers=headers)
+    with open('captcha.jpg', 'wb') as f:
+        f.write(response.content)
+        f.close()
+    im = Image.open('captcha.jpg')
+    im.show()
+    im.close()
+    captcha = input('captcha code： ')
+    return captcha
+
+
+def get_captcha0():
+    captchacode = soup.find('input', {'name': 'captcha_0'})['value']
+    return captchacode
+
+
+def login(email, password):
+    # Post the login imformation to the server
+    data = {
+        'csrfmiddlewaretoken': get_csrf(),
+        'email': email,
+        'password': password,
+        'captcha_1': get_captcha1(),
+        'captcha_0': get_captcha0(),
+        }
+    result = s.post(all_url, data=data, headers=headers)
+    s.cookies.save(ignore_discard=False, ignore_expires=False)
+
+
+# To verfy the login
+def is_login():
+    purl = 'http://readfree.me/accounts/profile/kinsluck/checkin/'
+    login_code = s.get(purl, headers=headers,
+                       allow_redirects=False).status_code
+    if login_code == 200:
+        return True
+    else:
+        return False
+
+
+def sign():
+    sign_url = 'http://readfree.me/accounts/checkin'
+    sign_code = requests.get(sign_url).status_code
+    if sign_code == 200:
+        return True
+        print('签到成功')
+    else:
+        return False
+        print('签到失败')
+
+
+if __name__ == '__main__':
+    if is_login():
+        print('您已经登录')
+        sign()
+        print('签到成功')
+    else:
+        account = input('请输入你的用户名\n>  ')
+        secret = input("请输入你的密码\n>  ")
+        login(account, secret)
